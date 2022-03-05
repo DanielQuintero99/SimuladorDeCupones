77406f7590d062537c94df45813767611004ea82
Daniel Quintero <daniel.manny10@hotmail.com>
2022-03-05 00:26:51 -0500

    add hof

--- script.js
+++ script.js
@@ -19,112 +19,174 @@ le pide al usuario que ingrese un cupon de descuento(dado al inicio para
 
 */
 
-const opciones=["1-Cargador 20.000\n","2-protector de pantalla 15.000\n","3-funda.10.000\n","4-Auriculares 150.000\n","5-Salir.\n"];
 
-class cupones{
-    constructor(nombre,descuento,porcentaje){
-        this.nombre=nombre;
-        this.descuento=descuento;
-        this.porcentaje=porcentaje;
+
+class cupones {
+    constructor(nombre, descuento, porcentaje) {
+        this.nombre = nombre;
+        this.descuento = descuento;
+        this.porcentaje = porcentaje;
     }
-    
-}
-let descuentos=[cupon20=new cupones("cupon20",0.20,": 20% de descuento\n"),cupon10=new cupones("cupon10",0.10,": 10% de descuento\n"),cupon5=new cupones("cupon5",0.05,": 5% de descuento\n")]
 
-class productos{
+}
+let descuentos = [cupon20 = new cupones("cupon20", 0.20, ": 20% de descuento\n"), cupon10 = new cupones("cupon10", 0.10, ": 10% de descuento\n"), cupon5 = new cupones("cupon5", 0.05, ": 5% de descuento\n")]
+
+class productos {
     constructor(nombre, precio) {
         this.nombre = nombre;
-        this.precio = precio; 
-        
+        this.precio = precio;
+
     }
- 
-    descuento(){
-        let palabra= prompt("si tienes un cupon, ingresalo aqui, de lo contrario pulsa enter.");
-        let cupon=palabra.toLowerCase();
-            if (cupon===cupon20.nombre) {    
-                this.precioDescuento=1;
-                return this.precioDescuento=this.precio-(this.precio*cupon20.descuento);     
-            }else if (cupon===cupon10.nombre) {
-                this.precioDescuento=1;
-                return this.precioDescuento=this.precio-(this.precio*cupon10.descuento); 
-            }else if (cupon===cupon5.nombre) {
-                this.precioDescuento=1;
-                return this.precioDescuento=this.precio-(this.precio*cupon5.descuento); 
-            }
-            else{
-                verificar=false;
-                alert("no ingresaste cupon");
-                
-            }
-    }
+
+    // descuento() {
+    //     let palabra = prompt("si tienes un cupon, ingresalo aqui, de lo contrario pulsa enter.");
+    //     let cupon = palabra.toLowerCase();
+
+    //     if (cupon === cupon20.nombre) {
+    //         this.precioDescuento = 1;
+    //         return this.precioDescuento = this.precio - (this.precio * cupon20.descuento);
+    //     } else if (cupon === cupon10.nombre) {
+    //         this.precioDescuento = 1;
+    //         return this.precioDescuento = this.precio - (this.precio * cupon10.descuento);
+    //     } else if (cupon === cupon5.nombre) {
+    //         this.precioDescuento = 1;
+    //         return this.precioDescuento = this.precio - (this.precio * cupon5.descuento);
+    //     } else {
+    //         verificar = false;
+    //         alert("no ingresaste cupon");
+
+    //     }
+
+    // }
 
 }
 
 
 let productosTotales = [cargador = new productos("cargador", 20000), protector = new productos("protector", 15000), funda = new productos("funda", 10000), auriculares = new productos("auriculares", 150000)];
- let verificar=true;
- let repetir=0;
- let listado="";
- let misCupones="";
-function listarProductos(){
-opciones.forEach((opcion)=>
-    listado+=opcion);
+let verificar = true;
+let listado = [];
+let misCupones = "";
+let carrito = [];
+
+function listarProductos() {
+    productosTotales.forEach((opcion) =>
+        listado += opcion.nombre + " " + opcion.precio + "\n");
 }
-function mostrarCupones(){
-    descuentos.forEach((descontar)=>    
-           misCupones+=descontar.nombre + " " + descontar.porcentaje
-                   
-   );
+
+function mostrarCupones() {
+    descuentos.forEach((descontar) =>
+        misCupones += descontar.nombre + " " + descontar.porcentaje
+
+    );
 }
+
+function agregarProductos() {
+
+    let seleccion = prompt(`Seleccione una opcion, escribe el producto que deseas : \n${listado}`);
+    const comprados = productosTotales.filter(x => {
+        return x.nombre === seleccion
+        
+    });
+    return carrito.push(comprados[0].precio); 
+    
+     
+}
+function validarCupones(){   
+        let palabra = prompt("si tienes un cupon, ingresalo aqui, de lo contrario pulsa enter.");
+        let cupon = palabra.toLowerCase();
+
+        if (cupon === cupon20.nombre) {
+            totalDescuento=total*cupon20.descuento;
+            alert(`tu descuento es de ${totalDescuento} el valor a pagar es de ${total-totalDescuento}`)
+        } else if (cupon === cupon10.nombre) {
+            totalDescuento=total*cupon10.descuento;
+            alert(`tu descuento es de ${totalDescuento} el valor a pagar es de ${total-totalDescuento}`)
+        } else if (cupon === cupon5.nombre) {
+            totalDescuento=total*cupon5.descuento;
+            alert(`tu descuento es de ${totalDescuento} el valor a pagar es de ${total-totalDescuento}`)
+        } else {
+            verificar = false;
+            alert("no ingresaste cupon");
+
+        }
+
+    }
+
+
+
+
 mostrarCupones();
 alert(misCupones)
 listarProductos();
-while (repetir!==5) {
+
+
+let bucle = true
+
+
     
-    repetir=parseInt(prompt(`Seleccione una opcion, digita el numero correspondiete: \n${listado}`));
-    if (repetir >= 1 && repetir <= 4) {
-        if (repetir === 1) {
-            cargador.descuento();
-            if (verificar===true) {           
-            alert(`Tu producto es ${cargador.nombre}, tu descuento es ${cargador.precio-cargador.precioDescuento} tu producto cuesta ${cargador.precioDescuento} `)
-        }else{
-            alert(`Tu producto es ${cargador.nombre}, tu producto cuesta ${cargador.precio} `)
-            verificar=true
-        }
-        } else if (repetir === 2) {
-            protector.descuento();
-            if (verificar===true) {           
-                alert(`Tu producto es ${protector.nombre}, tu descuento es ${protector.precio-protector.precioDescuento} tu producto cuesta ${protector.precioDescuento} `)
-            }else{
-                alert(`Tu producto es ${protector.nombre}, tu producto cuesta ${protector.precio} `)
-                verificar=true
-            }
-        } else if (repetir === 3) {
-            funda.descuento();
-            if (verificar===true) {           
-                alert(`Tu producto es ${funda.nombre}, tu descuento es ${funda.precio-funda.precioDescuento} tu producto cuesta ${funda.precioDescuento} `)
-            }else{
-                alert(`Tu producto es ${funda.nombre}, tu producto cuesta ${funda.precio} `)
-                verificar=true
-            }
-
-        } else if (repetir === 4) {
-            auriculares.descuento();
-            if (verificar===true) {           
-                alert(`Tu producto es ${auriculares.nombre}, tu descuento es ${auriculares.precio-auriculares.precioDescuento} tu producto cuesta ${auriculares.precioDescuento} `)
-            }else{
-                alert(`Tu producto es ${auriculares.nombre}, tu producto cuesta ${auriculares.precio} `)
-                verificar=true
-            }
-
-        }
-    } else if(repetir===5){
-        alert("Saliste");
-    }else {
-        alert("Ingresa un valor valido");
+agregarProductos(); 
+let total=0; 
+while (bucle === true) {
+    let agregarMas = parseInt( prompt("Deseas agregar mas productos? \n 1.SI \n 2.NO "));    
+                
+    if (agregarMas === 1) {   
+            
+        agregarProductos();
+           for (let suma of carrito) {  
+               total+=suma               
+           }
+           
+    } else if (agregarMas === 2) {
+        alert(`El total a pagar es de ${total}`)
+        bucle = false
+    }else{
+        alert("Ingresa un valor valido")
     }
 }
+validarCupones();
 
+// while (repetir !== 5) {
 
+//     repetir = parseInt(prompt(`Seleccione una opcion, escribe el priducto que deseas : \n${listado} O escribe "salir" para terminar `));
+//     if (repetir >= 1 && repetir <= 4) {
+//         if (repetir === 1) {
+//             cargador.descuento();
+//             if (verificar === true) {
+//                 alert(`Tu producto es ${cargador.nombre}, tu descuento es ${cargador.precio-cargador.precioDescuento} tu producto cuesta ${cargador.precioDescuento} `)
+//             } else {
+//                 alert(`Tu producto es ${cargador.nombre}, tu producto cuesta ${cargador.precio} `)
+//                 verificar = true
+//             }
+//         } else if (repetir === 2) {
+//             protector.descuento();
+//             if (verificar === true) {
+//                 alert(`Tu producto es ${protector.nombre}, tu descuento es ${protector.precio-protector.precioDescuento} tu producto cuesta ${protector.precioDescuento} `)
+//             } else {
+//                 alert(`Tu producto es ${protector.nombre}, tu producto cuesta ${protector.precio} `)
+//                 verificar = true
+//             }
+//         } else if (repetir === 3) {
+//             funda.descuento();
+//             if (verificar === true) {
+//                 alert(`Tu producto es ${funda.nombre}, tu descuento es ${funda.precio-funda.precioDescuento} tu producto cuesta ${funda.precioDescuento} `)
+//             } else {
+//                 alert(`Tu producto es ${funda.nombre}, tu producto cuesta ${funda.precio} `)
+//                 verificar = true
+//             }
 
+//         } else if (repetir === 4) {
+//             auriculares.descuento();
+//             if (verificar === true) {
+//                 alert(`Tu producto es ${auriculares.nombre}, tu descuento es ${auriculares.precio-auriculares.precioDescuento} tu producto cuesta ${auriculares.precioDescuento} `)
+//             } else {
+//                 alert(`Tu producto es ${auriculares.nombre}, tu producto cuesta ${auriculares.precio} `)
+//                 verificar = true
+//             }
 
+//         }
+//     } else if (repetir === 5) {
+//         alert("Saliste");
+//     } else {
+//         alert("Ingresa un valor valido");
+//     }
+// }
