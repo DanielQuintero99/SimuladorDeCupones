c065d2fa31a478d7b6528f0a4b99de10e75a3408
Daniel Quintero <daniel.manny10@hotmail.com>
2022-03-23 18:14:17 -0500

    adding seetAlert toDo fix bug on ForEach

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 mostrarAddProductos();
-generarCupones();
+cuponesSweetAlert();
 cargarCarrito();
 let carrito = cargarCarrito();
 let misCupones = "";
 
 function cargarCarrito() {
   let contenidoEnStorage = JSON.parse(localStorage.getItem("carritoEnStorage"));
-   if (contenidoEnStorage) {
+  if (contenidoEnStorage) {
     let array = [];
     for (let i = 0; i < contenidoEnStorage.length; i++) {
       let producto = new Producto(
@@ -14,24 +14,24 @@ function cargarCarrito() {
         contenidoEnStorage[i].cantidad
       );
       producto.ActualizarPrecioTotal();
-      let array= [...array,producto];
+      let array = [...array, producto];
       console.log(array)
-         
+
     }
     return array;
   }
-  
+
   return [];
 }
 
 
 
 function mostrarAddProductos() {
-    const nodoProductos = document.getElementById("gridProductos");
-    productosTotales.forEach((opcion) => {
-        const card = document.createElement("div")
-        card.setAttribute("class", "card");
-        card.innerHTML = `<div class="d-flex flex-column align-items-center justify-content-center">
+  const nodoProductos = document.getElementById("gridProductos");
+  productosTotales.forEach((opcion) => {
+    const card = document.createElement("div")
+    card.setAttribute("class", "card");
+    card.innerHTML = `<div class="d-flex flex-column align-items-center justify-content-center">
             <h2 class="card-title">${opcion.nombre}</h2>
             <div class="ContenedorImagen">
             <img class="img-fluid" style="height: 250px; width: 250px;" src="${opcion.img}">
@@ -39,39 +39,39 @@ function mostrarAddProductos() {
             <p class="card-text text-center">${opcion.precio}$</p>
             <a href="#" id="añadir${opcion.id}" class="btn btn-success btnAñadir">Añadir</a>
             </div>`;
-        nodoProductos.appendChild(card);
-        const btnAdd=document.getElementById(`añadir${opcion.id}`);
-        btnAdd.addEventListener("click",()=>agregarAlCarrito(opcion.id));
-       
-     });
+    nodoProductos.appendChild(card);
+    const btnAdd = document.getElementById(`añadir${opcion.id}`);
+    btnAdd.addEventListener("click", () => agregarAlCarrito(opcion.id));
+
+  });
 }
 
-function agregarAlCarrito(idProducto){
+function agregarAlCarrito(idProducto) {
 
-   let itemCarrito=carrito.find((elemento)=>elemento.id==idProducto);
- 
-   if (itemCarrito) {
-       let index= carrito.findIndex((elemento)=>elemento.id===itemCarrito.id);
-       carrito[index].add();
-       carrito[index].ActualizarPrecioTotal();
-       
-   }else{
-       carrito.push(new Producto (productosTotales[idProducto],1));
-      
-   }
- 
-   localStorage.setItem("carritoEnStorage",JSON.stringify(carrito));
-   mostrarCarrito(carrito)
+  let itemCarrito = carrito.find((elemento) => elemento.id == idProducto);
+
+  if (itemCarrito) {
+    let index = carrito.findIndex((elemento) => elemento.id === itemCarrito.id);
+    carrito[index].add();
+    carrito[index].ActualizarPrecioTotal();
+
+  } else {
+    carrito.push(new Producto(productosTotales[idProducto], 1));
+
+  }
+
+  localStorage.setItem("carritoEnStorage", JSON.stringify(carrito));
+  mostrarCarrito(carrito)
 }
 
-function mostrarCarrito(lista){
-    let divCarrito=document.getElementById("carrito");
-    divCarrito.innerHTML="";
-    let precioTotal=obtenerPrecioTotal(lista);
-    let table= document.createElement("div");
-    let total=document.getElementById("total");
-    total.innerHTML=`${precioTotal}`
-    table.innerHTML=`
+function mostrarCarrito(lista) {
+  let divCarrito = document.getElementById("carrito");
+  divCarrito.innerHTML = "";
+  let precioTotal = obtenerPrecioTotal(lista);
+  let table = document.createElement("div");
+  let total = document.getElementById("total");
+  total.innerHTML = `${precioTotal}`
+  table.innerHTML = `
     <table id="bodyTabla" class="table">
   <thead>
     <tr>
@@ -83,13 +83,13 @@ function mostrarCarrito(lista){
 </table>
 `;
 
-divCarrito.appendChild(table);
-let btnEliminar=document.getElementById("btnEliminar");
-    btnEliminar.addEventListener("click",borrarCarrito)
-let bodyTabla=document.getElementById("bodyTabla");
-    lista.forEach((deseos) => {
-        let datos=document.createElement("tbody");
-        datos.innerHTML=`
+  divCarrito.appendChild(table);
+  let btnEliminar = document.getElementById("btnEliminar");
+  btnEliminar.addEventListener("click", borrarCarrito)
+  let bodyTabla = document.getElementById("bodyTabla");
+  lista.forEach((deseos) => {
+    let datos = document.createElement("tbody");
+    datos.innerHTML = `
         
     <tr>
       <th scope="row">${deseos.cantidad}</th>
@@ -98,73 +98,97 @@ let bodyTabla=document.getElementById("bodyTabla");
       <td><button id="eliminar${deseos.id}" type="button" class=" botonQuitar btn-dark" >-</button></td>
     </tr>          
      `;
-     bodyTabla.appendChild(datos);
-     let eliminarUnidad=document.getElementById(`eliminar${deseos.id}`);
-     eliminarUnidad.addEventListener("click",()=>{
-       eliminarUnItem(deseos.id);
-     })     
+    bodyTabla.appendChild(datos);
+    let eliminarUnidad = document.getElementById(`eliminar${deseos.id}`);
+    eliminarUnidad.addEventListener("click", () => {
+      eliminarUnItem(deseos.id);
     })
-    
+  })
+
 }
 
-function obtenerPrecioTotal(){
-    return carrito.reduce((total,elemento)=>total+elemento.precioTotal,0);    
+function obtenerPrecioTotal() {
+  return carrito.reduce((total, elemento) => total + elemento.precioTotal, 0);
 }
 
-function eliminarUnItem(id){
-    let producto=carrito.find((producto)=>producto.id==id);
-    let index=carrito.findIndex((elemento)=>{
-      if(elemento.id===producto.id){
-        return true
-      }
-    });
-    if(producto.cantidad>1){
-      carrito[index].decrease();
-      carrito[index].ActualizarPrecioTotal();
-    }else{
-      carrito.splice(index,1);
+function eliminarUnItem(id) {
+  let producto = carrito.find((producto) => producto.id == id);
+  let index = carrito.findIndex((elemento) => {
+    if (elemento.id === producto.id) {
+      return true
     }
-    localStorage.setItem("carritoEnStorage",JSON.stringify(carrito));
-    mostrarCarrito(carrito)
+  });
+  if (producto.cantidad > 1) {
+    carrito[index].decrease();
+    carrito[index].ActualizarPrecioTotal();
+  } else {
+    carrito.splice(index, 1);
+  }
+  localStorage.setItem("carritoEnStorage", JSON.stringify(carrito));
+  mostrarCarrito(carrito)
 }
 
-function borrarCarrito(){
+function borrarCarrito() {
   carrito = [];
   mostrarCarrito(carrito);
 
 }
 
-function generarCupones() {
-    const nodoBody = document.getElementById("main");
-    const divCupones = document.createElement("div");
-    const tituloCupones=document.createElement("h3");
-    const listaCuponesTotales = document.createElement("ul");
-    listaCuponesTotales.setAttribute("class","list-group list-group-flush")
-    const botonSalir=document.createElement("button");
-    botonSalir.setAttribute("class","btn btn-info text-white");
-    tituloCupones.innerHTML="Disfruta de estos Cupones";
-    botonSalir.innerHTML="Salir";
-    nodoBody.appendChild(divCupones);
-    divCupones.appendChild(tituloCupones)
-    divCupones.appendChild(listaCuponesTotales);
-    divCupones.appendChild(botonSalir);
-    divCupones.setAttribute("class", "divCupones");
-    divCupones.style.display= "none";
-    const btnMostrarCupones = document.getElementById("btnCupones");
-    btnMostrarCupones.addEventListener("click",function(){
-        divCupones.style.display= "";
-    })
-    botonSalir.addEventListener("click", function(){
-        divCupones.style.display= "none";
-    })
-    descuentos.forEach((listaCupones) => {
-        const nodoLi=document.createElement("li");
-        nodoLi.setAttribute("class","list-group-item item-info")
-        nodoLi.innerHTML=listaCupones.nombre + listaCupones.porcentaje;
-        listaCuponesTotales.appendChild(nodoLi);        
-    })
+// function generarCupones() {
+//     const nodoBody = document.getElementById("main");
+//     const divCupones = document.createElement("div");
+//     const tituloCupones=document.createElement("h3");
+//     const listaCuponesTotales = document.createElement("ul");
+//     listaCuponesTotales.setAttribute("class","list-group list-group-flush")
+//     const botonSalir=document.createElement("button");
+//     botonSalir.setAttribute("class","btn btn-info text-white");
+//     tituloCupones.innerHTML="Disfruta de estos Cupones";
+//     botonSalir.innerHTML="Salir";
+//     nodoBody.appendChild(divCupones);
+//     divCupones.appendChild(tituloCupones)
+//     divCupones.appendChild(listaCuponesTotales);
+//     divCupones.appendChild(botonSalir);
+//     divCupones.setAttribute("class", "divCupones");
+//     divCupones.style.display= "none";
+//     const btnMostrarCupones = document.getElementById("btnCupones");
+//     btnMostrarCupones.addEventListener("click",function(){
+//         divCupones.style.display= "";
+//     })
+//     botonSalir.addEventListener("click", function(){
+//         divCupones.style.display= "none";
+//     })
+//     descuentos.forEach((listaCupones) => {
+//         const nodoLi=document.createElement("li");
+//         nodoLi.setAttribute("class","list-group-item item-info")
+//         nodoLi.innerHTML=listaCupones.nombre + listaCupones.porcentaje;
+//         listaCuponesTotales.appendChild(nodoLi);        
+//     })
+// }
+
+function  generarCupones(){
+ return descuentos.forEach((element)=> element.nombre)
 }
 
+function cuponesSweetAlert() {
+  let cuponesMostrados=generarCupones(); 
+  let nodo = document.getElementById("btnCupones");
+  nodo.addEventListener("click", () => {
+    Swal.fire({
+      title: 'Difruta de estos cupones',
+      text: cuponesMostrados,
+      width: 600,
+      padding: '3em',
+      color: 'black',
+      background: '#fff',
+      backdrop: `
+    rgba(0,0,0,0.7)
+    url("https://pa1.narvii.com/6547/6ff6730ac7ae0ceaac2c00664f0016d794af4859_hq.gif")
+    left top
+    no-repeat
+  `
+    })
+  })
+}
 
 
 
